a5eeec0f3ab983a5036e811932db7b713777102e
fly <woshenming@126.com>
2020-09-30 11:12:43 +0800

    fist commit in dev

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -6,6 +6,7 @@
 
 public class Git {
     public static void main(String[] args) {
-        System.out.println("args = " + args);
+        System.out.println("args = ");
+        System.out.println("____________");
     }
 }
---
+++ src/main/java/Git01.java
@@ -0,0 +1,11 @@
+/**
+ * @author 沈鸣
+ * 2020/9/30
+ */
+
+
+public class Git01 {
+    public static void main(String[] args) {
+        System.out.println("+++++++++");
+    }
+}
